44eed12361417cd04b12230eabc7e60d980dcfde
rid0n3 <zulmzmz@MockingBird.local>
2014-10-18 16:36:08 +0800

    file betol

---
+++ TVShows3/TVListTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  TVListTableViewController.h
+//  TVShows3
+//
+//  Created by ME-Tech Mac User 1 on 10/18/14.
+//  Copyright (c) 2014 ME-Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVListTableViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *menuBarButtonItem;
+
+@end
